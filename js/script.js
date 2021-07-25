ef5a80482a4c2a52d5510947d8d894713ad7ebd3
Pankaj <priyamkashyap555@gmail.com>
2021-07-25 10:10:12 +0530

    First commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function showMenu() {
+    var res = document.getElementById("nav-links");
+    res.style.right = "0";
+}
+function closeMenu() {
+    var res = document.getElementById("nav-links");
+    res.style.right = "-204px";
+}
